67a7e436a8586ea68c09cec2d12f81a1c9240211
Prakash S <79446554+prakashs-dev@users.noreply.github.com>
2023-11-20 14:34:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ function getFormvalue() {
   let ln = document.querySelector('input[name="Last Name"]').value;
   let number = document.querySelector('input[name="Phone Number"]').value;
   let email = document.querySelector('input[name="Email ID"]').value;
-	
-  alert("First Name: "+fn+"Last Name : "+ln+"Phone Number: "+number+"Email ID: "+email);
+	alert(`First Name: ${fn} 
+Last Name: ${ln}
+Phone Number: ${number}
+Email ID: ${email}`);
 }
