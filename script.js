3ec0a44b9ad3ecb7f7e5c5ab8824b40a1e40edb9
Prakash S <79446554+prakashs-dev@users.noreply.github.com>
2023-11-20 14:14:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function getFormvalue() {
+  let fn = document.myform.fname.value;
+  let ln = document.myform.lname.value;
+  let number = document.myform.phone.value;
+  let email = document.myform.mail.value;
+  alert("First Name: "+fn+"\nLast Name : "+ln+"\nPhone Number: "+number+"\nEmail ID: "+email);
+}
